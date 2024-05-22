400b1ed4b069d417985dc23821392de8e17689a6
devilhash <saikartheek352@gmail.com>
2024-05-22 12:42:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Sidebar from './Sidebar'
+import Home from './Home'
+import Products from './Products'
+import { useState } from 'react'
+import  './style.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App() {    
+  
+                    const [toggle, setToggle] = useState(true)    
+                    const Toggle = () => 
+                    {        setToggle(!toggle)    } 
+                  
+return (    
+<Router>
+<div className='container-fluid bg-secondary min-vh-100 '>       
+<div className='row '>            
+    {toggle && <div className='col-4 col-md-2 bg-white vh-100 position-fixed'>               
+ <Sidebar />            
+    </div>}           
+     {toggle &&  <div className='col-4 col-md-2'></div>}            
+        <div className='col'> 
+        <Routes>
+            
+              <Route path="/home" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+            
+            </Routes>
+           <Home Toggle={Toggle}/>           
+     </div>        
+    </div>    
+  </div> 
+  </Router>
+   )
 }
-
-export default App;
+export default App
---
+++ src/Products.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsListCheck, BsMenuButtonWideFill, BsFillGearFill } from 'react-icons/bs';
+
+
+export default function Products() {
+  return (
+    <main className='main-container'>
+        <div className='main-title'>
+            <h3>DASHBOARD</h3>
+        </div>
+
+        <div className='main-cards'>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>PRODUCTS</h3>
+                    <BsFillArchiveFill className='card_icon'/>
+                </div>
+                <h1>300</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>CATEGORIES</h3>
+                    <BsFillGrid3X3GapFill className='card_icon'/>
+                </div>
+                <h1>12</h1>
+            </div>
+            </div>
+            </main>
+  )
+}
